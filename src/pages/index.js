e42c02148ad5474d07e64f1ffe8431c3872cb72a
vishal <vishal.gxm@gmail.com>
2023-03-28 02:25:49 +0530

    added new Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { default as GenerateBill } from "./GenerateBill";
 export { default as Print } from "./Print";
 export { default as PrintRemark } from "./PrintRemark";
 export { default as Bill } from "./Bill";
+export { default as Jobs } from "./Jobs";
+export { default as AddJob } from "./AddJob";
