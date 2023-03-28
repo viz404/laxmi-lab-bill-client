99a6f9a764bf39a591b263c65204b9ace3828d25
vishal <vishal.gxm@gmail.com>
2023-03-28 21:53:51 +0530

    changed styling and restructured code

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -14,12 +14,14 @@ import {
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import calculatePriceV2 from "../calculationHelpers/calculatePriceV2";
 
-import DoctorSelector from "../components/DoctorSelector";
+import { useNavigate } from "react-router-dom";
+
 import JobWorkSelector from "../components/JobWorkSelector";
 import { addJobHandler } from "../reduxStore/jobs/jobActions";
+
+import calculatePriceV2 from "../calculationHelpers/calculatePriceV2";
+import DoctorSelectorRadio from "../components/DoctorSelectorRadio";
 
 const AddJob = () => {
   const [date, setDate] = useState("");
@@ -127,9 +129,9 @@ const AddJob = () => {
 
   return (
     <Box padding={10}>
-      <Flex gap={20} justifyContent="center">
-        <Flex direction="column" gap={5} width="15vw">
-          <DoctorSelector selectDoctor={updateDoctor} />
+      <Flex gap={20} justifyContent="center" alignItems="flex-start">
+        <Flex direction="column" gap={5} minWidth="15vw">
+          <DoctorSelectorRadio selectDoctor={updateDoctor} />
           <JobWorkSelector
             typeOfWorks={doctor?.typeOfWorks || []}
             selectTypeOfWork={selectWork}
@@ -145,127 +147,133 @@ const AddJob = () => {
             Cancel
           </Button>
         </Flex>
-        <Box width="45vw" borderWidth={1} borderRadius={10} boxShadow="md">
-          <Table>
-            <Tbody>
-              <Tr>
-                <Th>
-                  <Text fontSize="md">Date</Text>
-                </Th>
-                <Td>
-                  <Input
-                    name="date"
-                    type="date"
-                    onChange={(e) => setDate(e.target.value)}
-                  />
-                </Td>
-              </Tr>
-              <Tr>
-                <Th>
-                  <Text fontSize="md">Job No</Text>
-                </Th>
-                <Td>
-                  <Input
-                    name="jobNo"
-                    type="number"
-                    placeholder="Enter job number"
-                    onChange={(e) => setJobNumber(Number(e.target.value))}
-                  />
-                </Td>
-              </Tr>
-              <Tr>
-                <Th>
-                  <Text fontSize="md">Patient Name</Text>
-                </Th>
-                <Td>
-                  <Input
-                    name="patientName"
-                    placeholder="Enter patient name"
-                    onChange={(e) => setPatientName(e.target.value)}
-                  />
-                </Td>
-              </Tr>
-              <Tr>
-                <Th>
-                  <Text fontSize="md">Doctor</Text>
-                </Th>
-                <Td>{doctor?.name}</Td>
-              </Tr>
-              <Tr>
-                <Th borderWidth={0}>
-                  <Text fontSize="md">Work Types</Text>
-                </Th>
-                <Td borderWidth={0}>
-                  <Text>{works.map((e) => e.title).join(", ")}</Text>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-      </Flex>
-      <Flex marginTop={20} justifyContent="center" gap={100}>
-        <Flex width="40vw" direction="column" gap={10}>
-          {works.map((el, index) => (
-            <Box
-              key={index}
-              width="40vw"
-              borderWidth={1}
-              borderRadius={10}
-              boxShadow="md"
-            >
-              <Table>
-                <Thead>
-                  <Tr>
-                    <Th colSpan={2} textAlign="center" borderWidth={0}>
-                      <Text fontSize="md">{el.title}</Text>
-                    </Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td borderRightWidth={1}>
-                      <Input
-                        name="topLeft"
-                        id={index}
-                        borderColor="blackAlpha.500"
-                        placeholder="Upper Left"
-                        onChange={updateWork}
-                      />
-                    </Td>
-                    <Td>
-                      <Input
-                        borderColor="blackAlpha.500"
-                        placeholder="Upper Right"
-                        name="topRight"
-                        id={index}
-                        onChange={updateWork}
-                      />
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td borderRightWidth={1} borderBottomWidth={0}>
-                      <Input
-                        borderColor="blackAlpha.500"
-                        placeholder="Lower Left"
-                        name="bottomLeft"
-                        id={index}
-                        onChange={updateWork}
-                      />
-                    </Td>
-                    <Td borderBottomWidth={0}>
-                      <Input
-                        borderColor="blackAlpha.500"
-                        placeholder="Lower Right"
-                        name="bottomRight"
-                        id={index}
-                        onChange={updateWork}
-                      />
-                    </Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </Box>
-          ))}
+        <Flex direction="column" gap={30} alignItems="center">
+          <Box width="45vw" borderWidth={1} borderRadius={10} boxShadow="md">
+            <Table>
+              <Tbody>
+                <Tr>
+                  <Th>
+                    <Text fontSize="md">Date</Text>
+                  </Th>
+                  <Td>
+                    <Input
+                      name="date"
+                      type="date"
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Th>
+                    <Text fontSize="md">Job No</Text>
+                  </Th>
+                  <Td>
+                    <Input
+                      name="jobNo"
+                      type="number"
+                      placeholder="Enter job number"
+                      value={jobNumber}
+                      onChange={(e) => setJobNumber(Number(e.target.value))}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Th>
+                    <Text fontSize="md">Patient Name</Text>
+                  </Th>
+                  <Td>
+                    <Input
+                      name="patientName"
+                      placeholder="Enter patient name"
+                      value={patientName}
+                      onChange={(e) => setPatientName(e.target.value)}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Th>
+                    <Text fontSize="md">Doctor</Text>
+                  </Th>
+                  <Td>{doctor?.name}</Td>
+                </Tr>
+                <Tr>
+                  <Th borderWidth={0}>
+                    <Text fontSize="md">Work Types</Text>
+                  </Th>
+                  <Td borderWidth={0}>
+                    <Text>{works.map((e) => e.title).join(", ")}</Text>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </Box>
+          <Flex width="40vw" direction="column" gap={10}>
+            {works.map((el, index) => (
+              <Box
+                key={index}
+                width="40vw"
+                borderWidth={1}
+                borderRadius={10}
+                boxShadow="md"
+              >
+                <Table>
+                  <Thead>
+                    <Tr>
+                      <Th colSpan={2} textAlign="center" borderWidth={0}>
+                        <Text fontSize="md">{el.title}</Text>
+                      </Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td borderRightWidth={1}>
+                        <Input
+                          name="topLeft"
+                          id={index}
+                          borderColor="blackAlpha.500"
+                          placeholder="Upper Left"
+                          value={el?.topLeft || ""}
+                          onChange={updateWork}
+                        />
+                      </Td>
+                      <Td>
+                        <Input
+                          borderColor="blackAlpha.500"
+                          placeholder="Upper Right"
+                          name="topRight"
+                          id={index}
+                          value={el?.topRight || ""}
+                          onChange={updateWork}
+                        />
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td borderRightWidth={1} borderBottomWidth={0}>
+                        <Input
+                          borderColor="blackAlpha.500"
+                          placeholder="Lower Left"
+                          name="bottomLeft"
+                          id={index}
+                          value={el?.bottomLeft || ""}
+                          onChange={updateWork}
+                        />
+                      </Td>
+                      <Td borderBottomWidth={0}>
+                        <Input
+                          borderColor="blackAlpha.500"
+                          placeholder="Lower Right"
+                          name="bottomRight"
+                          id={index}
+                          value={el?.bottomRight || ""}
+                          onChange={updateWork}
+                        />
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </Box>
+            ))}
+          </Flex>
         </Flex>
       </Flex>
     </Box>
